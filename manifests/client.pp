ad9265e1ef495ba5b6faa0609b78d9b0c9437f2b
James Glenn <thedonkdonk@gmail.com>
2013-09-09 03:00:00 -0500

    Actually works every time...

--- manifests/client.pp
+++ manifests/client.pp
@@ -24,7 +24,7 @@ class icinga::client ($icinga_host) {
         alias => "$::hostname",
         address => "$::ipaddress",
         use => "linux-server",
-        target => "/etc/icinga/hosts/${::hostname}.cfg"
+        target => "/etc/icinga/hosts.cfg"
     }
 # Service Checks
     @@nagios_service { "check_users_${::hostname}":
@@ -33,7 +33,7 @@ class icinga::client ($icinga_host) {
         host_name => "$::fqdn",
         check_command => "check_nrpe!check_users",
         service_description => "Current Users",
-        target => "/etc/icinga/hosts/${::hostname}.cfg"
+        target => "/etc/icinga/services.cfg"
     }
     @@nagios_service { "check_load_${::hostname}":
         ensure => present,
@@ -41,7 +41,7 @@ class icinga::client ($icinga_host) {
         host_name => "$::fqdn",
         check_command => "check_nrpe!check_load",
         service_description => "Current Load",
-        target => "/etc/icinga/hosts/${::hostname}.cfg"
+        target => "/etc/icinga/services.cfg"
     }
     @@nagios_service { "check_zombie_procs_${::hostname}":
         ensure => present,
@@ -49,7 +49,7 @@ class icinga::client ($icinga_host) {
         host_name => "$::fqdn",
         check_command => "check_nrpe!check_zombie_procs",
         service_description => "Zombie Processes",
-        target => "/etc/icinga/hosts/${::hostname}.cfg"
+        target => "/etc/icinga/services.cfg"
     }
     @@nagios_service { "check_total_procs_${::hostname}":
         ensure => present,
@@ -57,7 +57,7 @@ class icinga::client ($icinga_host) {
         host_name => "$::fqdn",
         check_command => "check_nrpe!check_total_procs",
         service_description => "Total Processes",
-        target => "/etc/icinga/hosts/${::hostname}.cfg"
+        target => "/etc/icinga/services.cfg"
     }
     @@nagios_service { "check_disks_${::hostname}":
         ensure => present,
@@ -65,7 +65,7 @@ class icinga::client ($icinga_host) {
         host_name => "$::fqdn",
         check_command => "check_nrpe!check_disk",
         service_description => "Disks",
-        target => "/etc/icinga/hosts/${::hostname}.cfg"
+        target => "/etc/icinga/services.cfg"
     }
     @@nagios_service { "check_ssh_${::hostname}":
         ensure => present,
@@ -73,6 +73,6 @@ class icinga::client ($icinga_host) {
         host_name => "$::fqdn",
         check_command => "check_ssh",
         service_description => "SSH",
-        target => "/etc/icinga/hosts/${::hostname}.cfg"
+        target => "/etc/icinga/services.cfg"
     }
 }

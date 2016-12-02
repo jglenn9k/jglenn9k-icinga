232fe49a7a264bf231408d6efd23c2f41e9a8d73
James Glenn <thedonkdonk@gmail.com>
2016-12-02 11:58:33 -0800

    Some templating.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,30 +1,13 @@
 # == Class: icinga
 #
-# Full description of class icinga here.
+# Setup and configure Icinga server. Leverages exported resources for configs. 
 #
 # === Parameters
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
 #
 # === Examples
 #
 #  class { 'icinga':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
 #  }
 #
 # === Authors
@@ -33,11 +16,13 @@
 #
 # === Copyright
 #
-# Copyright 2015 James Glenn, unless otherwise noted.
+# Copyright 2016 James Glenn, unless otherwise noted.
 #
 class icinga (
     $package_name = 'icinga',
     $service_name = 'icinga',
+    $user_name = 'icinga',
+    $group_name = 'icinga',
     )
     {
     case $::osfamily {

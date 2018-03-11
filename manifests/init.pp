381ff0473d0324f7e566b138ac42cc7c496f2fe3
James Glenn <thedonkdonk@gmail.com>
2018-03-10 16:28:27 -0800

    Options for performance tuning.

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,30 +16,34 @@
 #
 # === Copyright
 #
-# Copyright 2017 James Glenn, unless otherwise noted.
+# Copyright 2018 James Glenn, unless otherwise noted.
 #
 class icinga (
-    $package_name = 'icinga',
-    $service_name = 'icinga',
-    $user_name    = 'icinga',
-    $group_name   = 'icinga',
-    $log_initial_states = '0',
-    $host_perfdata_file_processing_command = 'process-host-perfdata-file',
-    $service_perfdata_file_processing_command = 'process-service-perfdata-file',
-    $host_perfdata_file_processing_interval = '15',
-    $service_perfdata_file_processing_interval = '15',
-    $log_file     = '/var/log/icinga/icinga.log'
+    Stdlib::Httpurl $repourl                    = 'http://packages.icinga.org/epel/$releasever/release/',
+    Stdlib::Httpurl $gpgkeyurl                  = 'http://packages.icinga.org/icinga.key',
+    $package_name                               = 'icinga',
+    $service_name                               = 'icinga',
+    $user_name                                  = 'icinga',
+    $group_name                                 = 'icinga',
+    $log_initial_states                         = '0',
+    $host_perfdata_file_processing_command      = 'process-host-perfdata-file',
+    $service_perfdata_file_processing_command   = 'process-service-perfdata-file',
+    $host_perfdata_file_processing_interval     = '15',
+    $service_perfdata_file_processing_interval  = '15',
+    $use_large_installation_tweaks              = '0',
+    $enable_environment_macros                  = '0',
+    $log_file                                   = '/var/log/icinga/icinga.log'
     )
 {
     case $::osfamily {
         'RedHat': {
             yumrepo { 'icinga-stable-release':
                 ensure   => 'present',
-                baseurl  => 'http://packages.icinga.org/epel/$releasever/release/',
+                baseurl  => "$repourl",
                 descr    => 'ICINGA (stable release for epel)',
                 enabled  => '1',
                 gpgcheck => '1',
-                gpgkey   => 'http://packages.icinga.org/icinga.key',
+                gpgkey   => "$gpgkeyurl",
             }
         }
         default: {
@@ -67,115 +71,115 @@ class icinga (
     }
     file { '/etc/icinga/icinga.cfg':
     ensure => 'file',
-        owner   => 'root',
-        group   => 'root',
+        owner   => "$user_name",
+        group   => "$group_name",
         mode    => '0644',
         content => template('icinga/icinga.cfg.erb'),
         notify  => Service['icinga']
     }
     file { '/etc/icinga/cgi.cfg':
         ensure  => 'file',
-        owner   => 'root',
-        group   => 'root',
+        owner   => "$user_name",
+        group   => "$group_name",
         mode    => '0644',
         content => template('icinga/cgi.cfg.erb'),
         notify  => Service['icinga']
     }
     Nagios_command <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_command.cfg'
     }
     Nagios_contact <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_contact.cfg'
     }
     Nagios_contactgroup <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_contactgroup.cfg'
     }
     Nagios_host <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_host.cfg'
     }
     Nagios_hostdependency <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_hostdependency.cfg'
     }
     Nagios_hostescalation <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_hostescalation.cfg'
     }
     Nagios_hostextinfo <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_hostextinfo.cfg'
     }
     Nagios_hostgroup <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_hostgroup.cfg'
     }
     Nagios_service <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_service.cfg'
     }
     Nagios_servicedependency <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_servicedependency.cfg'
     }
     Nagios_serviceescalation <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_serviceescalation.cfg'
     }
     Nagios_serviceextinfo <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_serviceextinfo.cfg'
     }
     Nagios_servicegroup <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_servicegroup.cfg'
     }
     Nagios_timeperiod <<||>> {
         notify => Service['icinga'],
-        owner  => 'root',
-        group  => 'root',
+        owner  => "$user_name",
+        group  => "$group_name",
         mode   => '0644',
         target => '/etc/icinga/conf.d/icinga_timeperiod.cfg'
     }
